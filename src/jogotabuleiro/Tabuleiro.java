8556064aa23b2ce6d810c21b036b710b82cc54a0
Felipe S Bezerra <felipecrf220@gmail.com>
2023-02-18 16:34:23 -0300

    Adicionado programacao defensiva na classe Tabuleiro

--- src/jogotabuleiro/Tabuleiro.java
+++ src/jogotabuleiro/Tabuleiro.java
@@ -7,6 +7,9 @@ public class Tabuleiro {
 	private Peca[][] pecas;
 
 	public Tabuleiro(int linhas, int colunas) {
+		if (linhas <= 0 || colunas <=0) {
+			throw new TabuleiroException("Erro criando o tabuleiro: E necessario que haja pelo menos 1 linha e 1 coluna. ");
+		}
 		this.linhas = linhas;
 		this.colunas = colunas;
 		pecas = new Peca[linhas][colunas];
@@ -16,29 +19,47 @@ public class Tabuleiro {
 		return linhas;
 	}
 
-	public void setLinhas(int linhas) {
-		this.linhas = linhas;
-	}
-
 	public int getColunas() {
 		return colunas;
 	}
 
-	public void setColunas(int colunas) {
-		this.colunas = colunas;
-	}
-
 	public Peca peca(int linha, int coluna) {
+		if(!posicaoExiste(linha, coluna)) {
+			throw new TabuleiroException("Posicao nao esta no tabuleiro");
+		}
 		return pecas[linha][coluna];
 	}
 
 	public Peca peca(Posicao posicao) {
+		if(!posicaoExiste(posicao)) {
+			throw new TabuleiroException("Posicao nao esta no tabuleiro");
+		}
 		return pecas[posicao.getLinha()][posicao.getColuna()];
 	}
 
 	public void colocarPeca(Peca peca, Posicao posicao) {
+		if(temUmaPeca(posicao)) {
+			throw new TabuleiroException("Ja tem uma peca na posicao " + posicao);
+		}
 		pecas[posicao.getLinha()][posicao.getColuna()] = peca;
 		peca.posicao = posicao;
+	}
+	
+	// Metodo privado que auxilia a descobrir de forma mais facil se uma posicao existe 
+	// na matriz de pecas informando uma linha e uma coluna
+	private boolean posicaoExiste(int linha, int coluna) {
+		return linha >= 0 && linha < this.linhas && coluna >= 0 && coluna < this.colunas;
+	}
+	
+	public boolean posicaoExiste(Posicao posicao) {
+		return posicaoExiste(posicao.getLinha(), posicao.getColuna());
+	}
+	
+	public boolean temUmaPeca(Posicao posicao) {
+		if(!posicaoExiste(posicao)) {
+			throw new TabuleiroException("Posicao nao esta no tabuleiro");
+		}
+		return peca(posicao) != null;
 	}
 
 }
